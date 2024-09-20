a4844067384cc84457acecac1268d7ad30adf5f0
breadbread1984 <breadbread1984@163.com>
2024-09-20 16:40:18 +0800

    update

--- download_datasets.sh
+++ download_datasets.sh
@@ -10,5 +10,14 @@ python3 create_dataset.py --smiles CC --bond_dist 1.2 &
 python3 create_dataset.py --smiles CC --bond_dist 1.3 &
 python3 create_dataset.py --smiles CC --bond_dist 1.4 &
 python3 create_dataset.py --smiles CC --bond_dist 1.5 &
+python3 create_dataset.py --smiles C --bond_dist 0.6 &
+python3 create_dataset.py --smiles C --bond_dist 0.7 &
+python3 create_dataset.py --smiles C --bond_dist 0.8 &
+python3 create_dataset.py --smiles C --bond_dist 0.9 &
 python3 create_dataset.py --smiles C --bond_dist 1.0 &
+python3 create_dataset.py --smiles C --bond_dist 1.1 &
+python3 create_dataset.py --smiles C --bond_dist 1.2 &
+python3 create_dataset.py --smiles C --bond_dist 1.3 &
+python3 create_dataset.py --smiles C --bond_dist 1.4 &
+python3 create_dataset.py --smiles C --bond_dist 1.5 &
 wait
